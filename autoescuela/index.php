54e07314f01b0e2a217c1a95d92362d4b86108b4
tferragut2-debug <tferragut2@alumnes.aulabalear.org>
2026-02-23 19:24:17 +0100

    index.php

--- autoescuela/index.php
+++ autoescuela/index.php
@@ -1,3 +1,8 @@
-<?php
-echo "<h1>Autoescuela</h1>";
-?>
+<h1>Autoescuela</h1>
+
+<ul>
+    <li><a href="clientes.php">Clientes</a></li>
+    <li><a href="profesores.php">Profesores</a></li>
+    <li><a href="vehiculos.php">Vehículos</a></li>
+    <li><a href="clases.php">Clases</a></li>
+</ul>
